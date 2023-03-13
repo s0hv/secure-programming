1d9baa8fae78e965ee96c04a25d9d646eda1d085
s0hv <s0hvaperuna@gachimuchi.men>
2023-03-13 12:10:37 +0200

    material ui

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  modularizeImports: {
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+  },
 }
 
 module.exports = nextConfig
---
+++ web/utils/theme.ts
@@ -0,0 +1,32 @@
+import {
+  createTheme,
+  responsiveFontSizes,
+} from '@mui/material/styles';
+
+import { Roboto } from '@next/font/google';
+
+/* istanbul ignore next */
+export const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
+
+export const theme = responsiveFontSizes(createTheme({
+  palette: {
+    mode: 'dark'
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+}));
