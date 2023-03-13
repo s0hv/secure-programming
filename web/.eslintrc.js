95973d6f301a2089cb250ad13ec801ee7a29e5bc
s0hv <s0hvaperuna@gachimuchi.men>
2023-03-13 15:56:42 +0200

    Eslint changes

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,25 +1,51 @@
 module.exports = {
   extends: [
+    'eslint:recommended',
     'next/core-web-vitals',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  ignorePatterns: [
+    'out/'
+  ],
+  env: {
+    browser: true,
+    es2020: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  globals: {
+    React: 'writable'
+  },
   rules: {
-    semi: ['error', 'always', { 'omitLastInOneLineBlock': true }],
-    'quotes': ['warn', 'single', {
-      'avoidEscape': true,
-      'allowTemplateLiterals': true
+    semi: ['error', 'always', { omitLastInOneLineBlock: true }],
+    quotes: ['warn', 'single', {
+      avoidEscape: true,
+      allowTemplateLiterals: true
     }],
     'jsx-quotes': ['error', 'prefer-single'],
-    'react/jsx-filename-extension': [1, { 'extensions': ['.jsx', '.tsx'] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx']}],
     'object-curly-spacing': ['warn', 'always', {
-      'arraysInObjects':  false,
-      'objectsInObjects':  false
+      arraysInObjects:  false,
+      objectsInObjects:  false
     }],
     'operator-linebreak': ['warn', 'after'],
     'no-unused-vars': ['warn', {
-      'argsIgnorePattern':  '^_',
-      'ignoreRestSiblings': true
+      argsIgnorePattern:  '^_',
+      ignoreRestSiblings: true
     }],
+    'quote-props': ['error', 'as-needed'],
+
     'react/react-in-jsx-scope': 'off'
   }
 };
