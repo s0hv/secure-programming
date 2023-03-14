9977008c27db4a56ae993a1fb846957a8ee58956
tunasoup <74015604+tunasoup@users.noreply.github.com>
2023-03-14 13:03:46 +0200

    Removed an unnecessary dependency

--- web/pages/profile.tsx
+++ web/pages/profile.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
-import { Box, Button, Container, TextField, Typography } from '@mui/material';
+import { Box, Button, Container, Stack, TextField, Typography } from '@mui/material';
 import { FC } from 'react';
-import { Stack } from '@mui/system';
 import { NavBar } from '@/components/NavBar';
 
 
