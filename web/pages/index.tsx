b42c3eb86c915164a9019ff653c2be813cd4bb1e
tunasoup <74015604+tunasoup@users.noreply.github.com>
2023-03-14 13:04:56 +0200

    Created an early landing page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,7 +1,49 @@
 import Head from 'next/head';
-import styles from '@/styles/Home.module.css';
 import { NavBar } from '@/components/NavBar';
+import { FC } from 'react';
+import { Button, Container, Skeleton, Stack, Typography } from '@mui/material';
 
+const Posts: FC = () => {
+  return <>
+    <Stack
+      direction='column'
+      justifyContent='space-evenly'
+      alignItems='center'
+      spacing={4}
+    >
+      <Skeleton
+        variant='rectangular'
+        width={510}
+        height={180}
+        animation={false}
+      />
+      <Skeleton
+        variant='rectangular'
+        width={510}
+        height={160}
+        animation={false}
+      />
+      <Skeleton
+        variant='rectangular'
+        width={510}
+        height={190}
+        animation={false}
+      />
+      <Skeleton
+        variant='rectangular'
+        width={510}
+        height={190}
+        animation={false}
+      />
+      <Skeleton
+        variant='rectangular'
+        width={510}
+        height={190}
+        animation={false}
+      />
+    </Stack>
+  </>;
+};
 
 export default function Home() {
   return (
@@ -10,7 +52,26 @@ export default function Home() {
         <title>App</title>
       </Head>
       <NavBar/>
-      <main className={styles.main} />
+      <Container
+        component='main'
+        maxWidth='xs'
+        sx={{
+          textAlign: 'center',
+          mb: 5
+        }}
+      >
+        <Typography
+          variant='h5'
+          sx={{ mb: 1 }}
+        >
+                  Latest posts
+        </Typography>
+        <Posts/>
+      </Container>
+      <Button
+        href='/'
+        sx={{ position: 'fixed', bottom: '05%', right: '25%', }}
+      >New post</Button>
     </>
   );
 }
