adfa519db65067c33375ad18e99e4a81f36351eb
tunasoup <74015604+tunasoup@users.noreply.github.com>
2023-03-20 19:44:49 +0200

    Fixed timestamp formatting

--- web/utils/utilities.ts
+++ web/utils/utilities.ts
@@ -1,4 +1,4 @@
 export const formatTimestamp = (timestamp: string) => {
   const t = new Date(timestamp);
-  return `${t.getHours()}:${t.getMinutes()} · ${t.getDate()}.${t.getMonth()}.${t.getFullYear()}`;
+  return `${t.getHours()}:${t.getMinutes()} · ${t.getDate()}.${t.getMonth() + 1}.${t.getFullYear()}`;
 };
