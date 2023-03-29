ef2d54e10cfc99808e2f293b40ca11f617ae5f8d
tunasoup <74015604+tunasoup@users.noreply.github.com>
2023-03-29 15:12:40 +0300

    Added backend call for signing up, added an error message, fields are required and validated on frontend, authenticated user is redirected to landing page

--- web/pages/signup.tsx
+++ web/pages/signup.tsx
@@ -1,40 +1,116 @@
 import Head from 'next/head';
-import { Box, Button, Container, TextField, Typography } from '@mui/material';
-import { FC } from 'react';
+import { Alert, Box, Button, Container, TextField, Typography } from '@mui/material';
+import { FC, FormEvent, useEffect, useState } from 'react';
 import { NavBar } from '@/components/NavBar';
 import Link from '@/components/Link';
+import { csrfHeader, invalidateCsrfToken, useCSRF } from '@/utils/useCsrf';
+import { handleResponse } from '@/types/api/utilities';
+import { FrontendUser } from '@/types/api/user';
+import { useQueryClient } from '@tanstack/react-query';
+import { useUser } from '@/utils/useUser';
+import { useRouter } from 'next/router';
 
 const SignUpForm: FC = () => {
+  const [alert, setAlert] = useState('');
+  const csrf = useCSRF();
+  const queryClient = useQueryClient();
+  const { setUser, isLoading, isAuthenticated } = useUser();
+  const router = useRouter();
+
+  const signUpUser = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+    const body: Record<string, unknown> = {};
+    formData.forEach((val, key) => {
+      body[key] = val;
+    });
+
+    fetch('http://localhost:8080/api/user/createaccount', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        ...csrfHeader(csrf),
+      },
+      body: JSON.stringify(body),
+    }).then(handleResponse<FrontendUser>())
+      .then(setUser)
+      .then(() => invalidateCsrfToken(queryClient))
+      .then(() => {
+        console.log('account created succesfully');
+        setAlert('');
+        return router.push('/');
+      })
+      .catch((e) => {
+        console.log(`account creation failed ${e}`);
+        setAlert(e.message);
+      });
+  };
+
   return (
-    <>
+    <Box
+      display='flex'
+      flexDirection='column'
+      component='form'
+      onSubmit={signUpUser}
+      sx={{
+        mb: 2,
+      }}
+    >
       <Typography
         variant='h4'
         sx={{ mb: 2 }}
       >
         Sign up
       </Typography>
+      {alert ? (
+        <Alert
+          severity='error'
+          sx={{ margin: 'auto' }}
+        >
+          Sign up failed; {alert}
+        </Alert>
+      ) : null}
       <TextField
+        required
+        InputLabelProps={{ required: false }}
         name='username'
         label='Username'
-        sx={{ mb: 1 }}
+        inputProps={{ maxLength: 32 }}
+        sx={{ mb: 2 }}
       />
       <TextField
+        required
+        InputLabelProps={{ required: false }}
         name='password'
         type='password'
         label='Password'
-        sx={{ mb: 1 }}
+        inputProps={{ minLength: 8, maxLength: 72 }}
+        sx={{ mb: 2 }}
       />
       <TextField
+        required
+        InputLabelProps={{ required: false }}
         name='email'
         label='Email'
-        sx={{ mb: 1 }}
+        type='email'
+        sx={{ mb: 2 }}
       />
-      <Button variant='contained'>Sign up</Button>
+      <Button variant='contained' type='submit' disabled={isLoading || isAuthenticated}>Sign up</Button>
 
-    </>
+    </Box>
   );
 };
 export default function Signup() {
+  const { isAuthenticated } = useUser();
+  const router = useRouter();
+
+  // Redirect an already logged-in user to the landing page
+  useEffect(() => {
+    if (isAuthenticated) router.push('/');
+  });
+
   return (
     <>
       <Head>
@@ -46,16 +122,7 @@ export default function Signup() {
         maxWidth='xs'
         sx={{ textAlign: 'center' }}
       >
-        <Box
-          display='flex'
-          flexDirection='column'
-          component='form'
-          sx={{
-            mb: 2,
-          }}
-        >
-          <SignUpForm />
-        </Box>
+        <SignUpForm />
         <Box flexDirection='row' component='form'>
           <Link href='/login'>Already have an account?</Link>
         </Box>
