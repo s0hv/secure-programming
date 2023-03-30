df12b81eb7c9c08f75770cc483e48e696e65fe02
tunasoup <74015604+tunasoup@users.noreply.github.com>
2023-03-30 16:40:14 +0300

    Posts have a delete button visible to authorized users, connected delete button to backend

--- web/components/Post.tsx
+++ web/components/Post.tsx
@@ -1,10 +1,40 @@
 import { FC } from 'react';
-import { Card, CardContent, CardHeader, Typography } from '@mui/material';
+import { Button, Card, CardActions, CardContent, CardHeader, Typography } from '@mui/material';
 import { type Post as PostProps } from '@/types/api/post';
 import { formatTimestamp } from '@/utils/utilities';
+import { useUser } from '@/utils/useUser';
+import { csrfHeader, invalidateCsrfToken, useCSRF } from '@/utils/useCsrf';
+import { useQueryClient } from '@tanstack/react-query';
+import { QueryKeys } from '@/utils/constants';
+import { handleResponse } from '@/types/api/utilities';
 
 
-export const Post: FC<PostProps> = ({ user, text, timestamp }) => {
+export const Post: FC<PostProps> = ({ user, text, timestamp, postId }) => {
+  const { user: currentUser } = useUser();
+
+
+  const csrf = useCSRF();
+  const queryClient = useQueryClient();
+
+  const deletePost = () => {
+    const suffix = currentUser?.admin ? `/admin` : ``;
+    fetch(`http://localhost:8080/api/posts/delete/${postId}${suffix}`, {
+      method: 'DELETE',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        ...csrfHeader(csrf),
+      },
+    }).then(handleResponse())
+      .then(() => invalidateCsrfToken(queryClient))
+      .then(() => {
+        return queryClient.invalidateQueries(QueryKeys.posts);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  };
+
   return (
     <Card sx={{ width: 500 }}>
       <CardHeader
@@ -16,6 +46,16 @@ export const Post: FC<PostProps> = ({ user, text, timestamp }) => {
           {text}
         </Typography>
       </CardContent>
+      <CardActions>
+        {(currentUser?.userId === user.userId || currentUser?.admin) && (
+          <Button
+            size='small'
+            color='primary'
+            onClick={deletePost}
+          >Delete
+          </Button>
+        )}
+      </CardActions>
     </Card>
   );
 };
