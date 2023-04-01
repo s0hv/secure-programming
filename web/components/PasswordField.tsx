5a825d013f8b77d34f638e38e34c8e98226d08a3
tunasoup <74015604+tunasoup@users.noreply.github.com>
2023-04-01 19:30:33 +0300

    Added password strength bar which uses zxcvbn

--- web/components/PasswordField.tsx
+++ web/components/PasswordField.tsx
@@ -1,14 +1,32 @@
-import { ComponentProps, FC } from 'react';
-import { TextField } from '@mui/material';
+import { ComponentProps, FC, useState } from 'react';
+import { Box, TextField } from '@mui/material';
+import PasswordStrengthBar from 'react-password-strength-bar';
 
-export const PasswordField: FC<{strict?: boolean} & ComponentProps<typeof TextField>> = ({ strict = false, ...props }) => {
+
+/**
+ * Password textfield with options for strict requirements for length and a strength meter.
+ * For strict requirements, the password must be at least 8 characters long and at most 72.
+ * The strength meter uses zxcvbn to check the strength of the password, which helps users
+ * to create stronger passwords and block commonly used passwords.
+ */
+export const PasswordField: FC<{strict?: boolean, meter?: boolean} & ComponentProps<typeof TextField>> = ({ strict = false, meter = false, ...props }) => {
+  const [password, setPassword] = useState('');
+
   return (
-    <TextField
-      required
-      InputLabelProps={{ required: false }}
-      type='password'
-      inputProps={strict ? { minLength: 8, maxLength: 72 } : {}}
-      {...props}
-    />
+    <>
+      <TextField
+        required
+        InputLabelProps={{ required: false }}
+        type='password'
+        inputProps={strict ? { minLength: 8, maxLength: 72 } : {}}
+        onChange={(e) => setPassword(e.target.value)}
+        {...props}
+      />
+      {meter && password.length > 0 ? (
+        <Box sx={{ ml: 1, mr: 1 }}>
+          <PasswordStrengthBar password={password} minLength={8} />
+        </Box>
+      ) : null}
+    </>
   );
 };
